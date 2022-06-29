4bf9b255c67a7bb694c0385c5d8ea30a1a2dbf85
Lyn <lynfinman@gmail.com>
2022-06-29 12:51:00 -0400

    commit tree c241d7b7c594da08d756a9855771d8b7dfb0e240

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    end_with?('.')
   end
 
   def question?
-
+    end_with?('?')
   end
 
   def exclamation?
-
+    end_with?('!')
   end
 
   def count_sentences
-
+    sentences = split(/[!?.] /)
+    sentences.length
   end
 end
+
+# puts 'hello!'.exclamation?
+# puts 'This, well, is a sentence. This is too!! And so is this, I think? Woo...'.count_sentences
